0e181be3c748eefff7caa7ff7ff1552e3f8c4d2c
Kenneth Whitfield <77504666+S11635372@users.noreply.github.com>
2021-11-29 09:02:03 -0500

    v 0.3
    added color values

--- .github/Pygamepractice.py
+++ .github/Pygamepractice.py
@@ -1,4 +1,4 @@
-# Pygame practice, Kenneth Whitfield, 11/29/21, 8:38AM, v0.1    
+# Pygame practice, Kenneth Whitfield, 11/29/21, 9:01AM, v0.3  
 
 import pygame, sys
 from pygame.locals import *
@@ -9,3 +9,10 @@ pygame.init()
 #create game window
 windowSurface = pygame.display.set_mode((500, 400),0, 32)
 pygame.display.set_caption ("Hello, world")
+
+#set collor values
+BLACK = (0, 0, 0) 
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
