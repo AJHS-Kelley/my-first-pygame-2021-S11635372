ec30e7668c48c24f726ce5dc2d2ffbb06fc40232
Kenneth Whitfield <77504666+S11635372@users.noreply.github.com>
2021-11-29 08:56:36 -0500

    v 0.2
    made window show up

---
+++ .github/Pygamepractice.py
@@ -0,0 +1,11 @@
+# Pygame practice, Kenneth Whitfield, 11/29/21, 8:38AM, v0.1    
+
+import pygame, sys
+from pygame.locals import *
+
+# start pygame
+pygame.init()
+
+#create game window
+windowSurface = pygame.display.set_mode((500, 400),0, 32)
+pygame.display.set_caption ("Hello, world")
