63802d610f051872e3af20f0797bdd97d20a30e9
Kenneth Whitfield <77504666+S11635372@users.noreply.github.com>
2021-12-09 08:46:11 -0500

    v0.2
    added directions and speed

--- .github/simple_animation.py
+++ .github/simple_animation.py
@@ -1,4 +1,4 @@
-#simple Animation with PyGame, Kenneth Whitfield, 12/9/21, 8:29 AM, V 0.1
+#simple Animation with PyGame, Kenneth Whitfield, 12/9/21, 8:45 AM, V 0.2
 
 import pygame, sys, time
 from pygame.locals import *
@@ -11,3 +11,11 @@ WINDOWWIDTH = 400
 WINDOWHEIGHT = 400
 windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
 pygame.display.set_caption('Animation Example!')
+
+#setup direction variables
+DOWNLEFT = 'downleft'
+DOWNRIGHT = 'downright'
+UPLEFT = 'upleft'
+UPRIGHT = 'upright'
+
+MOVESPEED = 4
