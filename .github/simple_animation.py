989d4b8fe43b4ac00dd969a96b1985a6aad3bca7
Kenneth Whitfield <77504666+S11635372@users.noreply.github.com>
2021-12-09 08:40:38 -0500

    V0.1
    Setup pygame

--- .github/simple_animation.py
+++ .github/simple_animation.py
@@ -1,1 +1,13 @@
-#simple Animation with PyGame, Kenneth Whitfield, 12/9/21, 8:29 AM, V 0.0
+#simple Animation with PyGame, Kenneth Whitfield, 12/9/21, 8:29 AM, V 0.1
+
+import pygame, sys, time
+from pygame.locals import *
+
+#Setup PyGame
+pygame.init()
+
+#setup the window
+WINDOWWIDTH = 400
+WINDOWHEIGHT = 400
+windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
+pygame.display.set_caption('Animation Example!')
