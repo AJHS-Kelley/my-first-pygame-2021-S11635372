f48e4b0e127c86e04757a4ee42a2d1b7f5f8d969
Kenneth Whitfield <77504666+S11635372@users.noreply.github.com>
2022-01-13 08:57:41 -0500

    v0.7
    added button commands

--- .github/collison_detection.py
+++ .github/collison_detection.py
@@ -1,4 +1,4 @@
-#pygame collison detection practice, Kenneth Whitfield, 1/13/22, 8:38 AM, v0.6
+#pygame collison detection practice, Kenneth Whitfield, 1/13/22, 8:57 AM, v0.7
 
 import pygame, sys, random
 from pygame.locals import *
@@ -57,3 +57,20 @@ while True:
             if event.key == K_DOWN or event.key == K_s:
                 moveUp = False
                 moveDown = True
+        if event.type == KEYUP:
+            if event.key == K_ESCAPE:
+                pygame.quit()
+                sys.exit()
+            #check to see if the player has stopped moving
+            if event.key == K_LEFT or event.key == K_a:
+                moveLeft = False
+            if event.key == K_RIGHT or event.key == K_d:
+                moveRight = False
+            if event.key == K_UP or event.key == K_w:
+                moveUp = False
+            if event.key == K_DOWN or event.key == K_s:
+                moveDown = False
+            if event.key == K_x: #use x to teleport the player.
+                player.top = random.randint(0, WINDOWHEIGHT - player.height)
+                player.left = random.randint(0, WINDOWWIDTH - player.width)
+            
